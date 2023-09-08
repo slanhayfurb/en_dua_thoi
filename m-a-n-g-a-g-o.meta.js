a194fc3805a9ac57bb1884ddfc08a2fc7ec60146
slanhayfurb <144471453+slanhayfurb@users.noreply.github.com>
2023-09-08 15:49:17 +0000

    cn

---
+++ m-a-n-g-a-g-o.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         m-a-n-g-a-g-o
+// @namespace    https://github.com/slanhayfurb/en_dua_thoi/raw/main/m-a-n-g-a-g-o.user.js
+// @version      2023.09.08
+// @description  down m-a-n-g-a-g-o
+// @author       You
+// @match        https://www.mangago.me/read-manga/*/*/*/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.mangago.me
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/slanhayfurb/en_dua_thoi/raw/main/m-a-n-g-a-g-o.user.js
+// @updateURL    https://github.com/slanhayfurb/en_dua_thoi/raw/main/m-a-n-g-a-g-o.meta.js
+// ==/UserScript==
